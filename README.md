9e04e7c7f183e314d584bc7d06ae1194c1fe7dc0
Ben Madelin <ben.madelin@ntlworld.com>
2020-11-09 19:58:58 +0000

    Contents of dictionary imported from file into global array.

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ with the Player attempting one letter at a time.***
 
 ## Checklist
 * [x] Obtain plaintext file of nouns in the english dictionary.
+* [x] Import words from file and select a random index from the list.
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hangman")
+def getFile():
+    wordList = []
+    with open("words.txt", "r") as file:
+        for line in file:
+            wordList.append(line.strip('\n'))
+
+    print(wordList)
+
+
+getFile()
