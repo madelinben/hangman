aca17fb60d90ef1d4058e3228515daf6bcf53795
Ben Madelin <ben.madelin@ntlworld.com>
2020-11-09 20:02:15 +0000

    Random word selected from dictionary list.

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import random
+
+
 def getFile():
     wordList = []
     with open("words.txt", "r") as file:
         for line in file:
             wordList.append(line.strip('\n'))
 
-    print(wordList)
+    randomIndex = random.choice(wordList)
+    return randomIndex
 
 
-getFile()
+print(getFile())
