8bd8305063eb9473a98ede69c09e420c7046b9a8
Ben Madelin <ben.madelin@ntlworld.com>
2020-11-09 20:06:59 +0000

    Selected word covered and initial game setup.

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import random
 
 
-def getFile():
+def getWord():
     wordList = []
     with open("words.txt", "r") as file:
         for line in file:
             wordList.append(line.strip('\n'))
 
-    randomIndex = random.choice(wordList)
+    randomIndex = random.choice(wordList).upper()
     return randomIndex
 
 
-print(getFile())
+def play():
+    attempts = 6
+    finished = False
+
+    selectedWord = getWord()
+    guessWord = "_" * len(selectedWord)
+
+    while not finished:
+        finished = True
+
+
+play()
