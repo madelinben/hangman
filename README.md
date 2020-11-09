9e6dbfa0d864b06073ddd70f9f1b96e85c119dd3
Ben Madelin <ben.madelin@ntlworld.com>
2020-11-09 21:09:17 +0000

    Guessed characters are validated and uncovered in the displayed attempts.

--- README.md
+++ README.md
@@ -5,4 +5,8 @@ with the Player attempting one letter at a time.***
 ## Checklist
 * [x] Obtain plaintext file of nouns in the english dictionary.
 * [x] Import words from file and select a random index from the list.
-* [x] Cover guessing word and setup initial game values. 
+* [x] Cover selected word for display.
+* [x] Implement game loop while user is guessing the word and has available attempts. 
+* [x] Validate whether an attempt is a single char and has not previously been made.
+* [x] Guessed characters are validated and uncovered to the user.
+* [x] Break game loop and confirm whether the covered word was guessed correctly.
--- main.py
+++ main.py
@@ -19,20 +19,34 @@ def play():
     coveredWord = "_" * len(selectedWord)
 
     while not guessedCorrectly and attemptCount > 0:
+        print(coveredWord)
 
-        validateAttempt = True
-        while validateAttempt:
+        validateInput = True
+        while validateInput:
             guess = input("Guess a character: ").upper()
             if len(guess) == 1 and guess.isalpha():
                 for char in coveredWord:
                     if char == guess:
-                        validateAttempt = False
+                        validateInput = False
                         print("Guess has already been attempted!")
             else:
-                validateAttempt = False
+                validateInput = False
                 print("Provide a character value for a valid attempt!")
 
+        validateAttempt = False
+        for char in selectedWord:
+            if char == guess:
+                coveredWord += char
+                validateAttempt = True
+            elif char.isalpha():
+                coveredWord += char
+            else:
+                coveredWord += "_"
 
+        if validateAttempt:
+            print("Correct Attempt! the character " + guess + " is located in the covered word.")
+        else:
+            print("Incorrect Attempt! the character " + guess + " is not located in the covered word.")
 
         if coveredWord == selectedWord:
             print("Congratulations! You've correctly guessed the covered word!")
@@ -40,6 +54,9 @@ def play():
             break
 
         attemptCount -= 1
+
+    if not guessedCorrectly:
+        print("Unlucky! You've run out of attempts and not guessed the covered word.\nThe word you were guessing was " + selectedWord)
 
 
 play()
