ec6dfd3d1d170f323f3bd4461777c7c6ae4165a5
Ben Madelin <ben.madelin@ntlworld.com>
2020-11-12 19:11:08 +0000

    Cleaned up code and simplified validation for more efficient execution.

--- main.py
+++ main.py
@@ -18,14 +18,14 @@ def play():
 
     selectedWord = getWord()
 
-    coveredWord = "_" * len(selectedWord)
+    coveredWord = ["_"] * len(selectedWord)
     previousGuesses = []
 
     while not guessedCorrectly and attemptCount > 0:
-        print(coveredWord + "\n" + selectedWord)
+        print("".join(coveredWord) + "\n" + selectedWord)
         guess = input("Guess a character: ").upper()
 
-        if len(guess)==1 and guess.isalpha():
+        if len(guess) == 1 and guess.isalpha():
             if guess in previousGuesses:
                 print("Guess has already been attempted!")
             elif guess not in selectedWord:
@@ -35,6 +35,19 @@ def play():
             else:
                 print("Correct Attempt! the character " + guess + " is located in the covered word.")
                 previousGuesses.append(guess)
+
+                for i in range(len(selectedWord)):
+                    if selectedWord[i] == guess:
+                        coveredWord[i] = guess
+
+                if "_" not in coveredWord:
+                    guessedCorrectly = True
+
+    if guessedCorrectly:
+        print("Congratulations! You've correctly guessed the covered word " + selectedWord + "!")
+    else:
+        print(
+            "Unlucky! You've run out of attempts and not guessed the covered word.\nThe word you were guessing was " + selectedWord + "!")
 
 
 play()
